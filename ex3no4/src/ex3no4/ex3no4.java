b99349e6604eea6eefb9f25edb3cf15121039802
admin <admin@DESKTOP-8L2IG92>
2021-04-11 06:43:44 +0900

    ex3no4

---
+++ ex3no4/src/ex3no4/ex3no4.java
@@ -0,0 +1,14 @@
+package ex3no4;
+
+public class ex3no4 {  //96p
+	public static void main(String[] args) {
+		int i = -10;
+		i = +i;
+		System.out.println(i);
+		
+		i = -10;
+		i = -i;
+		System.out.println(i);
+	}
+
+}
